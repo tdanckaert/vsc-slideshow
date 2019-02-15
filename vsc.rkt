f1441078fa8913c747c6f68290a9ef4a557fb2e1
Thomas Danckaert <thomas.danckaert@uantwerpen.be>
2019-02-15 14:48:33 +0100

    Improve fonts and colors.

--- vsc.rkt
+++ vsc.rkt
@@ -3,20 +3,26 @@
 (require pict racket/draw ppict/2)
 
 (define flanders-art
-  (make-font #:face "Flanders Art Sans,Regular"
+  (make-font #:face "FlandersArtSans-Regular"
 	     #:size 32))
 
 (define flanders-art-light
   (make-font #:face "Flanders Art Sans,Light"
 	     #:size 32))
 
-(define flanders-art-large
-  (make-font #:face "Flanders Art Sans,Regular"
-	     #:size 35))
+(define flanders-art-medium
+  (make-font #:face "Flanders Art Sans,Medium"
+	     #:size 32))
+
+(define flanders-art-regular-bold
+  (make-font #:face "FlandersArtSans-Regular"
+	     #:size 32
+	     #:weight 'ultraheavy))
 
 (define flanders-art-small
-  (make-font #:face "Flanders Art Sans,Regular"
-	     #:size 25))
+  (make-font #:face "FlandersArtSans-Regular"
+	     #:size 28))
+
 
 (define helvetica
   (make-font #:face "Helvetica,Regular"
@@ -26,7 +32,7 @@
 (define vsc-orange
   (make-color 219 108 48))
 (define vsc-dark
-  (make-color 55 54 54))
+  (make-color 78 88 101))
 (define vsc-gray
   (make-color 246 246 246))
 (define vsc-background
@@ -36,11 +42,11 @@
   (colorize
    (vr-append
     (clip-descent (text "VLAAMS" flanders-art-light))
-    (clip-descent (text "SUPERCOMPUTER"  flanders-art))
-    (text "CENTRUM" flanders-art))
+    (clip-descent (text "SUPERCOMPUTER"  flanders-art-medium))
+    (text "CENTRUM" flanders-art-medium))
    color))
 
-(define (trapeze w)
+(define (trapeze w h)
   (dc (lambda (dc dx dy)
 	(define old-brush (send dc get-brush))
 	(define old-pen (send dc get-pen))
@@ -48,25 +54,25 @@
 	      (new brush% [style 'solid] [color vsc-orange]))
 	(send dc set-pen (new pen% [style 'transparent]))
 	(define path (new dc-path%))
-	(send path move-to 0 20)
+	(send path move-to 0 (/ h 2))
 	(send path line-to w 0)
-	(send path line-to w 100)
-	(send path line-to 0 100)
+	(send path line-to w h)
+	(send path line-to 0 h)
 	(send path close)
 	(send dc draw-path path dx dy)
 	(send dc set-brush old-brush)
 	(send dc set-pen old-pen))
-      w 100))
+      w h))
 
 (define vsc-bg
   (let ([w (+ (* 2 margin) client-w)]
 	[h (+ (* 2 margin) client-h)])
     (inset (rb-superimpose
 	    (filled-rectangle w h #:draw-border? #f #:color vsc-background)
-	    (trapeze w)
+	    (trapeze w 80)
 	    (inset (scale-to-fit (vsc-logo)
-				 (rectangle 80 80) #:mode 'preserve/max)
-		   0 0 20 10))
+				 (rectangle 60 60) #:mode 'preserve/max)
+		   0 0 20 8))
 	   (- margin))))
 
 (define (mysub s . rest)
@@ -74,7 +80,7 @@
 
 (current-titlet
  (lambda (s)
-   (colorize (text s flanders-art-large 50) vsc-dark)))
+   (colorize (text s flanders-art-medium 50) vsc-dark)))
 
 (current-slide-assembler
  (lambda (title v-sep content)
